f695042dced2b3cedd72cce21b5afa59b1ea629e
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-09-22 10:03:08 -0700

    Task completed

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints the result of the addition of all arguments"""
+    import sys
+    adds = 0
+    num_arg = sys.argv
+
+    for i in range(1, len(num_arg)):
+        adds++ = int(num_arg[i])
+    print(adds)i
