92236db17865bbb325a89ac43681a36dab354d75
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-09-22 09:49:56 -0700

    Task completed

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Program adds a, b and return the result"""
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
