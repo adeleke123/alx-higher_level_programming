51eef2840c14b84934f5d643b6af5b8b8a8f7939
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-26 21:21:49 +0100

    Task completed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+
+
 def new_in_list(my_list, idx, element):
-    """Replaces an element in a list at a specific position
-    without modifying the original list like in C."""
-    
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
     new_list = my_list.copy()
