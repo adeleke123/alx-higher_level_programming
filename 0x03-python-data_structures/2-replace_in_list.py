d2625838305a189061db2033f7188bf2d260ddd4
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-26 20:56:19 +0100

    Task completed

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+
 def replace_in_list(my_list, idx, element):
-    """Replaces an element of a list at a specific position like in C"""
-    
     if idx < 0 or idx >= len(my_list):
         return my_list
     my_list[idx] = element
