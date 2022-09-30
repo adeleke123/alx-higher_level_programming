2a6eaa23dcd9ed4d08e1e220e818f01eae8e3872
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-30 11:55:24 +0100

    Task completed

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,5 @@
-#!/usr
+#!/usr/bin/python3
+def update_dictionary(a_dict, key, value):
+    """Returns a new dictionary with all values multiplied by 2"""
+    a_dict[key] = value
+    return a_dict
