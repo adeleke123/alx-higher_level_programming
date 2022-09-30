c9147657978b19703dbcd70feede6adfc9c02d2c
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-30 11:15:37 +0100

    Task completed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    add = 0
-    for i in set(my_list):
-        add += i
-    return add
+def search_replace(my_list, search, replace):
+    """Function that replaces all occurrences of an element by another
+    in a new list."""
+
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
