da52af3242a5506bc1ce6dd1154ff00cd076ffe5
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-30 11:17:21 +0100

    Task completed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,4 @@
-#!/bin/usr
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
