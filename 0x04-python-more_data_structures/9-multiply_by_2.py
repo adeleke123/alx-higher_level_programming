01303328e4dfd59c31b9c9b94d7376c3c35ce229
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-30 12:01:06 +0100

    Task completed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,4 @@
-#!/bin/bash
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """Returns a new dictionary with all values multiplied by 2"""
+    return {key: i*2 for key, i in a_dictionary.items()}
