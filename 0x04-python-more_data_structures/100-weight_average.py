d4f80a184a87451daf67c03d4852219029820c92
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-30 11:18:33 +0100

    Task completed

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,11 @@
-#!/bin?usr
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """Returns the weighted average of all integers tuple."""
+    if my_list and len(my_list):
+        x = 0
+        y = 0
+        for tups in my_list:
+            x += (tups[0] * tups[1])
+            y += tups[1]
+        return (x / y)
+    return 0
