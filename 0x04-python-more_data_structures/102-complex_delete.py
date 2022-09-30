0b72b8297de9ef8b8fd68401cbae4bbc1021a0ea
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-30 11:21:10 +0100

    Task completed

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,9 @@
-#!/bin/bash
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    """Delete keys with a specific value in a dictionary."""
+    dict_keys = list(a_dictionary.keys())
+
+    for dict_value in dict_keys:
+        if value == a_dictionary.get(dict_value):
+            del a_dictionary[dict_value]
+    return (a_dictionary)
