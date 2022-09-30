0ad0263024c93b69bd9dd806d5af5b1510f6e9e7
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-09-30 11:27:32 +0100

    Task completed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,24 @@
-#!/bin/bash
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """Converts a Roman numeral to an integer."""
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
+
+    if type(roman_string) != str or roman_string is None:
+        return 0
+
+    nums = [roman_dict[i] for i in roman_string]
+    result = 0
+
+    for i in range(len(nums)):
+        result += nums[i]
+        if nums[i - 1] < nums[i] and i != 0:
+            result -= (nums[i - 1] + nums[i - 1])
+    return result
