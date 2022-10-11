3f63d650b124d2b0e1ce30856913080b87330a41
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-10-11 03:08:36 +0100

    Task completed

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
+    """Function that does exactly the same as Python bytecode"""
     result = 0
-
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('Too far')
-
-            result += a ** b / i
-        except:
+                raise Exception("Too Far")
+            else:
+                result += (a ** b) / i
+        except (Exception):
             result = b + a
             break
-
     return result
