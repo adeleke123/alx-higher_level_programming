67929c6e86aead3d43482790cf295bec2a401d41
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-10-19 15:52:18 +0100

    Task completed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,11 @@
-sg
+#!/usr/bin/python3
+"""LockedClass"""
+
+
+class LockedClass:
+    """
+    A class LockedCass that prevents the user from dynamically
+    creating new instance attributes, except if the new instance
+    attribute is called first_name
+    """
+    __slots__ = "first_name"
