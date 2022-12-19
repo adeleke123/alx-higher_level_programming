1ce460f607655f250726f9a717ad219aad14ae90
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-19 15:15:08 -0800

    Task completed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);  /* Get the arguments passed to the script, excluding the first two elements (which are the path to Node.js and the path to the script) */
+const args = process.argv.slice(2);
 if (args.length === 0) {
   console.log('No argument');
 } else if (args.length === 1) {
