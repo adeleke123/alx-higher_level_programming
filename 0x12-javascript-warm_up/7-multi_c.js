a491e52a9ac2221f16947a5b748179c7e1175ce2
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-20 08:56:24 -0800

    Task completed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const x = process.argv[2];
-if (isNaN(x)) {
-  console.log("Missing number of occurrences");
-} else {
+const x = Number(process.argv[2]);
+if (x) {
   for (let i = 0; i < x; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
