a66a375095d0034a15e3a547239103b11d043dda
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-20 06:48:21 -0800

    Task completed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const firstArgument = process.argv[2];
-
 const isNumber = !isNaN(firstArgument);
-
 if (isNumber) {
   console.log(`My number: ${parseInt(firstArgument, 10)}`);
 } else {
