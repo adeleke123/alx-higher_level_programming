29f290f7a703ea2289818b5d1c0befcd98cc6034
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-20 09:22:24 -0800

    Task completed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2], 10);
-if (isNaN(size)) {
-  console.log('Missing size');
-  return;
-}
-for (let i = 0; i < size; i++) {
-  let row = '';
-  for (let j = 0; j < size; j++) {
-    row += 'X';
+const size = Number(process.argv[2]);
+if (size) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
   }
-  console.log(row);
+} else {
+  console.log('Missing size');
 }
