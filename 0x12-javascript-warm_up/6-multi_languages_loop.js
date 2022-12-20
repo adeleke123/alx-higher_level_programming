a6cc24804c20b0a2b9a296a08ae563f6b008d4d3
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-20 07:04:11 -0800

    Task completed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const message of messages) {
   console.log(message);
 }
