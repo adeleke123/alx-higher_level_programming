6c0f152f6a4af39efdcb5668679ab38156414fa3
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-23 02:53:38 -0800

    Task completed

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width * this.height;
+    }
+}
