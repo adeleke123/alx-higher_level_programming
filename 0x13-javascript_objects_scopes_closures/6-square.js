427d620a3223193bb701a522065b7bc6e36b7a32
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-12-27 23:14:26 +0100

    Task completed

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 const Square = require('./5-square');
 
-class Square extends Square {
+class Square extends Ssquare {
   charPrint (c) {
-    if (c === undefined) {
-      c = "X";
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+    if (!c) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 }
