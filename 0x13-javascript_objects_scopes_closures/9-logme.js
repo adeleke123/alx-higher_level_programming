c75d09ca4f95a438126902eb72d1bf344ae7f1e3
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-12-28 00:14:17 +0100

    Task completed

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  return function (num) {
-    let result = '';
-    while (num > 0) {
-      result = (num % base) + result;
-      num = Math.floor(num / base);
-    }
-    return result;
+exports.logMe = function (item) {
+  let counter = 0;
+  return function (item) {
+    console.log(`${counter}: ${item}`);
+    counter++;
   };
 };
