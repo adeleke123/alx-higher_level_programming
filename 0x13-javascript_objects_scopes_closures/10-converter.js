97554d84f9b3388ae238ec22aa0076288076d93f
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-12-28 00:11:29 +0100

    Task completed

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,11 +1,2 @@
 #!/usr/bin/node
-exports.converter = function (base) {
-  return function (num) {
-    let result = '';
-    while (num > 0) {
-      result = (num % base) + result;
-      num = Math.floor(num / base);
-    }
-    return result;
-  };
-};
+exports.converter = function (base) { return num => num.toString(base); };
