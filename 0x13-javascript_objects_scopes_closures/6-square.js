512491da5ba54a5756912efea069aa2d0eb8fafc
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-27 13:58:12 -0800

    Task completed

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class Square extends Square {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
