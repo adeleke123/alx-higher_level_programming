e47c58f829b4038a22fcf8a7fa17b27ff161a280
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-27 14:53:39 -0800

    Task completed

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  return function(num) {
+    let result = '';
+    while (num > 0) {
+      result = (num % base) + result;
+      num = Math.floor(num / base);
+    }
+    return result || '0';
+  };
+};
