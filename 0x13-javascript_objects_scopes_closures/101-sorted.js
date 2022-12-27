84b56551ea962ebcf5572a82c7aba9c85497436b
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-12-27 15:34:45 -0800

    Task completed

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
+  }
+  newDict[occurrences].push(userId);
+}
+console.log(newDict);
