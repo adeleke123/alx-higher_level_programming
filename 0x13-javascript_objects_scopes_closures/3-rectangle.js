89770887f798017a43da55f615a958a3c50a9da9
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-12-27 22:15:07 +0100

    Task completed

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return;
     }
+    this.width = w;
+    this.height = h;
   }
-  
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
