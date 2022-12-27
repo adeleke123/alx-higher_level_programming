fd7c91946966b4933369c87034e72e5c0bebc555
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2022-12-27 19:13:47 +0100

    Task completed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,9 @@
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return;
-    }
     this.width = w;
     this.height = h;
+    }
   }
 }
 module.exports = Rectangle;
