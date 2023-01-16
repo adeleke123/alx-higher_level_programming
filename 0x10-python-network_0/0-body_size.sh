307357b25031bf5038596190b87c263ffeb75ad5
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-16 23:21:20 +0100

    Task completed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends request to it while requesting its body-size-response
+# Takes in a URL, sends request to it while requesting its body-size-response.
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
