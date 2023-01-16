1a4403db724c18179717730719caa8fe1a4e9428
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-16 23:11:16 +0100

    Task completed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """Find a peak model"""
+
+
 def find_peak(list_of_integers):
-    def helper(arr, low, high, n):
-        mid = low + (high - low) // 2
-        if ((mid == 0 or arr[mid-1] <= arr[mid]) and
-            (mid == n-1 or arr[mid+1] <= arr[mid])):
-            return mid
-        elif (mid > 0 and arr[mid-1] > arr[mid]):
-            return helper(arr, low, (mid-1), n)
-        else:
-            return helper(arr, (mid+1), high, n)
-    n = len(list_of_integers)
-    return list_of_integers[helper(list_of_integers, 0, n-1, n)]
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
