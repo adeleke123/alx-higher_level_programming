179485fb46bd501e0e844d4ac405a62aff4662cb
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-17 22:57:17 +0100

    Backend

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
+"""Sends a request to a given URL and displays the response body
 Usage: ./3-error_code.py <URL>
   - Handles HTTP errors.
 """
