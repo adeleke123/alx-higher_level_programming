03b93e47ead95f5edc31ff213587f64c78294fda
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-17 23:06:04 +0100

    Backend

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
+"""Displays the X-Request-Id header variable of a request to a given URL
 Usage: ./5-hbtn_header.py <URL>
 """
 import sys
 import requests
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
