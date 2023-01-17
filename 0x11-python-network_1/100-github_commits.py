439836fb87ac0b841135e38283bb0e6ad400d143
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-17 23:51:35 +0100

    Backend

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
+"""Lists the 10 most recent commits on a given GitHub repository
 Usage: ./100-github_commits.py <repository name> <repository owner>
 """
 import sys
 import requests
-
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
