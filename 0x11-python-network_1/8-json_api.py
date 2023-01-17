329e16ca9c433c8802baa4d34e271b0eae775fc1
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-17 23:27:56 +0100

    Backend

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter
 Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
   - If no letter is provided, sends `q=""`.
 """
 import sys
 import requests
-
 
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
