f844388009cd0bbc968493b6e3142f08d6895e4e
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-17 23:12:29 +0100

    Backend

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
+"""Sends a POST request to a given URL with a given email
 Usage: ./6-post_email.py <URL> <email>
   - Displays the body of the response.
 """
 import sys
 import requests
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
