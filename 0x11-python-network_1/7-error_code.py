8d010022f1923e465e7d422242de9e3ae1fb03ba
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-17 23:17:13 +0100

    Backend

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
+"""Sends a request to a given URL and displays the response body
 Usage: ./7-error_code.py <URL>
   - Handles HTTP errors.
 """
 import sys
 import requests
-
 
 if __name__ == "__main__":
     url = sys.argv[1]
