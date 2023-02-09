01d0b051cfa8c7b1981bd68639ff0115573303bc
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-02-09 22:52:40 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,7 @@
-gdhfhfhfh
+#!/usr/bin/node
+// script writes a string to a file
+
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
